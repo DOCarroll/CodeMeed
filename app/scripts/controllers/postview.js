1b4f36f2d8dab0816b9beb2fa063169ddcaf8496
Danny O'Carroll <LemonMyth@live.com>
2015-07-30 16:58:07 -0400

    Made Design Styling

--- app/scripts/controllers/postview.js
+++ app/scripts/controllers/postview.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('PostViewCtrl', function ($scope, $routeParams, Post, Profile, Auth, $location) {
+app.controller('PostViewCtrl', function ($firebase, $scope, $routeParams, Post, Profile, Auth, $location) {
 	$scope.posts = Post.all;
 	$scope.post = Post.get($routeParams.postId);
 	$scope.comments = Post.comments($routeParams.postId);
@@ -28,26 +28,26 @@ app.controller('PostViewCtrl', function ($scope, $routeParams, Post, Profile, Au
 		$scope.comments.$add(comment);
 
 		$scope.commentText = '';
-		if($scope.post.language == '1'){
+		if($scope.post.language == 'Python'){
 			var userScore = profileRef.child('devscorePython');
 			userScore.transaction(function(devscorePython){
 				return devscorePython+5;
 			});
 
 		}
-		else if($scope.post.language == '2'){
+		else if($scope.post.language == 'Php'){
 			var userScore = profileRef.child('devscorePhp');
 			userScore.transaction(function(devscorePhp){
 				return devscorePhp+5;
 			});
 		}
-		else if($scope.post.language == '3'){
+		else if($scope.post.language == 'Ruby'){
 			var userScore = profileRef.child('devscoreRuby');
 			userScore.transaction(function(devscoreRuby){
 				return devscoreRuby+5;
 			});
 		}
-		else if($scope.post.language == '4'){	
+		else if($scope.post.language == 'Javascript'){	
 			var userScore = profileRef.child('devscoreJavascript');
 			userScore.transaction(function(devscoreJavascript){
 				return devscoreJavascript+5;
@@ -70,34 +70,41 @@ app.controller('PostViewCtrl', function ($scope, $routeParams, Post, Profile, Au
 	$scope.complete = function (post) {
 		var profileRefBase = new Firebase('https://intense-inferno-7123.firebaseio.com/profile/');
 		var profileRef = profileRefBase.child($scope.user.uid);
-		
-		if($scope.post.language == '1'){
+
+		var postBase = new Firebase('https://intense-inferno-7123.firebaseio.com/posts/');
+		var posts = $firebase(postBase).$asArray();
+		console.log(posts);
+		console.log(post);
+		if($scope.post.language == 'Python'){
 			var userScore = profileRef.child('devscorePython');
 			userScore.transaction(function(devscorePython){
 				return devscorePython+100;
 			});
 
 		}
-		else if($scope.post.language == '2'){
+		else if($scope.post.language == 'Php'){
 			var userScore = profileRef.child('devscorePhp');
 			userScore.transaction(function(devscorePhp){
 				return devscorePhp+100;
 			});
 		}
-		else if($scope.post.language == '3'){
+		else if($scope.post.language == 'Ruby'){
 			var userScore = profileRef.child('devscoreRuby');
 			userScore.transaction(function(devscoreRuby){
 				return devscoreRuby+100;
 			});
 		}
-		else if($scope.post.language == '4'){	
+		else if($scope.post.language == 'Javascript'){	
 			var userScore = profileRef.child('devscoreJavascript');
 			userScore.transaction(function(devscoreJavascript){
 				return devscoreJavascript+100;
 			});
+			
+			
 		}
 
-		Post.delete($scope.post);
+		return posts.$remove(post.child($scope.postId));
+		
 	};
 
 });
