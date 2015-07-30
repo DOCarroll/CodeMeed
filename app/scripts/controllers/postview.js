7bf1219063a9c119cb4948a3d1302755e0ff8a28
Danny O'Carroll <LemonMyth@live.com>
2015-07-30 01:29:11 -0400

    Created Award Functionality, still needs to delete on reward

--- app/scripts/controllers/postview.js
+++ app/scripts/controllers/postview.js
@@ -1,8 +1,10 @@
 'use strict';
 
 app.controller('PostViewCtrl', function ($scope, $routeParams, Post, Profile, Auth, $location) {
+	$scope.posts = Post.all;
 	$scope.post = Post.get($routeParams.postId);
 	$scope.comments = Post.comments($routeParams.postId);
+	$scope.postId = $routeParams.postId;
 
 	$scope.user = Auth.user;
 	$scope.signedIn = Auth.signedIn;
@@ -63,6 +65,39 @@ app.controller('PostViewCtrl', function ($scope, $routeParams, Post, Profile, Au
 	$scope.deleteComment = function (comment) {
 		$scope.comments.$remove(comment);
 
+	};
+
+	$scope.complete = function (post) {
+		var profileRefBase = new Firebase('https://intense-inferno-7123.firebaseio.com/profile/');
+		var profileRef = profileRefBase.child($scope.user.uid);
+		
+		if($scope.post.language == '1'){
+			var userScore = profileRef.child('devscorePython');
+			userScore.transaction(function(devscorePython){
+				return devscorePython+100;
+			});
+
+		}
+		else if($scope.post.language == '2'){
+			var userScore = profileRef.child('devscorePhp');
+			userScore.transaction(function(devscorePhp){
+				return devscorePhp+100;
+			});
+		}
+		else if($scope.post.language == '3'){
+			var userScore = profileRef.child('devscoreRuby');
+			userScore.transaction(function(devscoreRuby){
+				return devscoreRuby+100;
+			});
+		}
+		else if($scope.post.language == '4'){	
+			var userScore = profileRef.child('devscoreJavascript');
+			userScore.transaction(function(devscoreJavascript){
+				return devscoreJavascript+100;
+			});
+		}
+
+		Post.delete($scope.post);
 	};
 
 });
